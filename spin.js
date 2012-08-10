6c7c60798d03db24c3c0fa3742b7e52c8165aa7e
Felix Gnass <fgnass@gmail.com>
2012-08-10 12:09:38 +0200

    Added parseInt for top and left. Closes #90 and #81

--- spin.js
+++ spin.js
@@ -158,8 +158,8 @@
         tp = pos(target)
         ep = pos(el)
         css(el, {
-          left: (o.left == 'auto' ? tp.x-ep.x + (target.offsetWidth >> 1) : o.left + mid) + 'px',
-          top: (o.top == 'auto' ? tp.y-ep.y + (target.offsetHeight >> 1) : o.top + mid)  + 'px'
+          left: (o.left == 'auto' ? tp.x-ep.x + (target.offsetWidth >> 1) : parseInt(o.left, 10) + mid) + 'px',
+          top: (o.top == 'auto' ? tp.y-ep.y + (target.offsetHeight >> 1) : parseInt(o.top, 10) + mid)  + 'px'
         })
       }
 
