2708531a3439e83b7ba0d7d8a1e5c75b5debb0ea
XhmikosR <xhmikosr@users.sourceforge.net>
2012-10-29 01:40:03 +0200

    combine javascript files and move their inclusion to the bottom

--- assets/minify.bat
+++ assets/minify.bat
@@ -7,4 +7,4 @@ pushd %~dp0
 cat main.css prettify.css fd-slider/fd-slider.css ^
  fd-slider/fd-slider-tooltip.css | cleancss -o style.css
 
-rem cat prettify.js fd-slider/fd-slider.js | uglifyjs -o pack.js
+cat prettify.js ../spin.js fd-slider/fd-slider.js | uglifyjs -o pack.js
