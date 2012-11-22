cb7938d9b12d1001690907edf69893490a593fc3
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-22 03:35:15 +0200

    use type in minify.bat and minify main.css last

--- assets/minify.bat
+++ assets/minify.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set PATH=C:\MSYS\bin;%PATH%
-
 pushd %~dp0
 
-cat main.css prettify.css fd-slider/fd-slider.css ^
- fd-slider/fd-slider-tooltip.css | cleancss -o style.css
+type prettify.css fd-slider\fd-slider.css ^
+ fd-slider\fd-slider-tooltip.css main.css | cleancss -o style.css
 
-cat prettify.js ../spin.js fd-slider/fd-slider.js | uglifyjs -o pack.js
+type prettify.js ..\spin.js fd-slider\fd-slider.js | uglifyjs -o pack.js
+
+popd
