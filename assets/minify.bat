d08354f0409e6a2398e66d9dc4f62c6d22f49a10
XhmikosR <xhmikosr@users.sourceforge.net>
2012-11-22 03:38:34 +0200

    switch to uglify-js v2.2.0

--- assets/minify.bat
+++ assets/minify.bat
@@ -5,6 +5,6 @@ pushd %~dp0
 type prettify.css fd-slider\fd-slider.css ^
  fd-slider\fd-slider-tooltip.css main.css | cleancss -o style.css
 
-type prettify.js ..\spin.js fd-slider\fd-slider.js | uglifyjs -o pack.js
+cmd /c uglifyjs prettify.js fd-slider\fd-slider.js ..\spin.js --compress --mangle -o pack.js
 
 popd
