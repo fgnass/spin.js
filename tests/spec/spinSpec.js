a08cf979d3dff870d19967831c3960f485ca0327
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-06-02 02:22:49 -0300

    Add test for length option

--- tests/spec/spinSpec.js
+++ tests/spec/spinSpec.js
@@ -30,11 +30,18 @@ test.describe('Sample Test', function() {
   });
 
   test.describe('Spin Test Page', function() {
-    test.it('should have 34 div elements when create a spinner with 17 lines', function() {
+    test.it('should have 34 div elements, when create a spinner as 17 lines', function() {
       driver.get( urlSpinTest );
       driver.executeScript( util.format(script, {lines: 17}, target) );
       driver.executeScript( "return $('.spinner div').length" ).then( function(result) {
         assert.equal( result, 34, "The number of lines should be 34. The double of 17." );
+      });
+    });
+    test.it('should have 12px of width, when create a spinner as 7 length', function() {
+      driver.get( urlSpinTest );
+      driver.executeScript( util.format(script, {length: 7}, target) );
+      driver.executeScript( "return $('.spinner div div').first().css('width')" ).then( function(result) {
+        assert.equal( result, "12px", "The width of spinner should be 12px. A sum of 5px plus 7 (length)." );
       });
     });
   });
