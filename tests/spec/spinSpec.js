e5516ccf63c7b62fc5dd70e1ee0ecb0f5dfb6a86
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-06-02 02:28:42 -0300

    Add test for width option

--- tests/spec/spinSpec.js
+++ tests/spec/spinSpec.js
@@ -30,18 +30,28 @@ test.describe('Sample Test', function() {
   });
 
   test.describe('Spin Test Page', function() {
-    test.it('should have 34 div elements, when create a spinner as 17 lines', function() {
+
+    test.it('should have 34 div elements, when create a spinner as {lines: 17}', function() {
       driver.get( urlSpinTest );
       driver.executeScript( util.format(script, {lines: 17}, target) );
       driver.executeScript( "return $('.spinner div').length" ).then( function(result) {
         assert.equal( result, 34, "The number of lines should be 34. The double of 17." );
       });
     });
-    test.it('should have 12px of width, when create a spinner as 7 length', function() {
+
+    test.it('should have 12px of width, when create a spinner as {length: 7}', function() {
       driver.get( urlSpinTest );
       driver.executeScript( util.format(script, {length: 7}, target) );
       driver.executeScript( "return $('.spinner div div').first().css('width')" ).then( function(result) {
         assert.equal( result, "12px", "The width of spinner should be 12px. A sum of 5px plus 7 (length)." );
+      });
+    });
+
+    test.it('should have 8px of height, when create a spinner as {width: 8}', function() {
+      driver.get( urlSpinTest );
+      driver.executeScript( util.format(script, {width: 8}, target) );
+      driver.executeScript( "return $('.spinner div div').first().css('height')" ).then( function(result) {
+        assert.equal( result, "8px", "The height of spinner should be 8px." );
       });
     });
   });
