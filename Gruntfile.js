de644c570d68833c6b99e4577f93bb02f68c93c8
Theodore Brown <theodorejb@outlook.com>
2017-12-03 10:47:18 -0600

    Add Content-Security-Policy header during development to ensure compatibility

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,28 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          port: 9080,
+          base: {
+            path: 'site',
+            options: {
+              // add CSP header to ensure spin.js supports it
+              setHeaders: function (res, path) {
+                let defaultSrc = "default-src 'self';";
+                let styleSrc = "style-src 'self' fonts.googleapis.com;";
+                let fontSrc = "font-src 'self' fonts.gstatic.com;";
+                let imgSrc = "img-src 'self' www.gravatar.com;";
+                res.setHeader('Content-Security-Policy', `${defaultSrc} ${styleSrc} ${fontSrc} ${imgSrc}`);
+              },
+            },
+          },
+        }
+      }
+    },
+
     copy: {
       js: {
         files: [
@@ -22,9 +44,11 @@ module.exports = function(grunt) {
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-gh-pages');
 
+  grunt.registerTask('serve', ['connect']);
   grunt.registerTask('default', ['copy']);
 
 };
