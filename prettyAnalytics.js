f66cf12a2be4634c69e086966da20ba686f2499e
Theodore Brown <theodorejb@outlook.com>
2017-12-10 22:10:49 -0600

    automatic commit

---
+++ prettyAnalytics.js
@@ -0,0 +1,8 @@
+prettyPrint();
+
+var _gaq = [['_setAccount', 'UA-19036385-2'], ['_trackPageview']];
+(function (d, t) {
+    var g = d.createElement(t), s = d.getElementsByTagName(t)[0]; g.async = 1;
+    g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s)
+}(document, "script"));
