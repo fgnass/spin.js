67b43feaa0d0eb6ead132fa2c297ce68808a0cdd
Theodore Brown <theodorejb@outlook.com>
2018-09-02 11:02:27 -0500

    automatic commit

--- bundle.js
+++ bundle.js
@@ -1,13 +1,16 @@
 (function () {
     'use strict';
 
-    var __assign = (undefined && undefined.__assign) || Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
+    var __assign = (undefined && undefined.__assign) || function () {
+        __assign = Object.assign || function(t) {
+            for (var s, i = 1, n = arguments.length; i < n; i++) {
+                s = arguments[i];
+                for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                    t[p] = s[p];
+            }
+            return t;
+        };
+        return __assign.apply(this, arguments);
     };
     var defaults = {
         lines: 12,
