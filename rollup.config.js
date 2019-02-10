f411ab9097da19a40a07b617d9a5f27791d82518
Theodore Brown <theodorejb@outlook.com>
2019-02-10 12:01:45 -0600

    Link to spin.umd.js as alternative for those who can't use ES6 modules
    Closes #367.
    
    The UMD file is not included in the npm package since it's unnecessary for anyone using a module bundler.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,13 @@
 export default [
     {
+        input: 'spin.js',
+        output: {
+            file: 'site/spin.umd.js',
+            format: 'umd',
+            name: 'Spin',
+        },
+    },
+    {
         input: 'site/index.js',
         output: {
             file: 'site/bundle.js',
