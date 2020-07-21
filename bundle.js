1bc9acd14ebf3c9c4d941889e5c19feaab38fa20
Theodore Brown <theodorejb@outlook.com>
2020-07-21 12:11:58 -0600

    automatic commit

--- bundle.js
+++ bundle.js
@@ -257,11 +257,58 @@
         if (shareEl.checked) {
             window.location.replace('#?' + getParamStringFromOpts(opts));
         }
+
+        let codeEl = document.getElementById('spinner-options');
+        codeEl.textContent = getOptionsCode(opts);
+        Prism.highlightElement(codeEl);
+    }
+
+    function getOptionsCode(options) {
+        var optDescriptions = {
+            lines: 'The number of lines to draw',
+            length: 'The length of each line',
+            width: 'The line thickness',
+            radius: 'The radius of the inner circle',
+            scale: 'Scales overall size of the spinner',
+            corners: 'Corner roundness (0..1)',
+            color: 'CSS color or array of colors',
+            fadeColor: 'CSS color or array of colors',
+            speed: 'Rounds per second',
+            rotate: 'The rotation offset',
+            animation: 'The CSS animation name for the lines',
+            direction: '1: clockwise, -1: counterclockwise',
+            zIndex: 'The z-index (defaults to 2e9)',
+            className: 'The CSS class to assign to the spinner',
+            top: 'Top position relative to parent',
+            left: 'Left position relative to parent',
+            shadow: 'Box-shadow for the lines',
+            position: 'Element positioning',
+        };
+
+        let code = "import {Spinner} from 'spin.js';\n\n";
+        code += "var opts = {\n";
+
+        for (let opt in options) {
+            let value = options[opt];
+
+            if (typeof value === 'string') {
+                value = `'${value}'`;
+            }
+
+            code += `  ${opt}: ${value}, // ${optDescriptions[opt]}\n`;
+        }
+
+        code += "};\n\n";
+
+        code += "var target = document.getElementById('foo');\n";
+        code += "var spinner = new Spinner(opts).spin(target);";
+
+        return code;
     }
 
     function getOptionsFromInputs() {
         var opts = {};
-        
+
         for (var i = 0; i < inputs.length; i++) {
             var input = inputs[i];
             var val = input.value;
@@ -273,10 +320,12 @@
             }
 
             opts[input.name] = val;
-
-            // set value in code usage example
-            document.getElementById('opt-' + input.name).textContent = val;
         }
+
+        // set all options so they can be shown in code example
+        opts['zIndex'] = 2e9;
+        opts['className'] = 'spinner';
+        opts['position'] = 'absolute';
 
         for (var i = 0; i < cbInputs.length; i++) {
             var input = cbInputs[i];
